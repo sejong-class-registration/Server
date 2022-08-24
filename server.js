4385eb954d8aaa16cdbc95f81c74f9e69f5ccfb1
cksrb63 <cksr1@naver.com>
2022-08-24 17:59:28 +0900

    [ADD] 폴더 구조화

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./App");
+
+// MongoDB 랑 연결 후, config.env 채우고 여기 주석 풀기
+
+// dotenv.config({ path: './config.env' });
+
+// const DB = process.env.DATABASE;
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log('DB connection successful!');
+//   });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}`);
+// });
