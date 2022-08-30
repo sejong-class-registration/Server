ccccd6bb158d2284c2422949311a90e62a4f4ed6
yyunee <mwg9503@naver.com>
2022-08-30 23:22:33 +0900

    add user lecture model

---
+++ models/lectureModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
+
+const lectureSchema = new mongoose.Schema(
+  {
+    name: {
+        type: String,
+        required: [true, '강의명이 필요합니다'],
+        minlength: 2,
+        maxlength: 50
+      },
+    lecture_id: {
+        type: String,
+        required: [true, '학수번호기 필요합니다'],
+        minlength: 2,
+        maxlength: 50
+      },
+    distrib: {//분반
+        type: String,
+        required: [true, '분반이 필요합니다'],
+        default: 0 
+      },
+    english: {
+        type: Boolean,
+        default:false
+      },
+    credit: {//학점
+        type: Number,
+        required: [true, '학점이 필요합니다'],
+        default: 0
+      },
+    lecture_grade: {
+        type: Number,
+        required: [true, '학년이 필요합니다'],
+        default: 0
+    },
+    department: { //주관학과
+        type: String,
+        required: [true, '주간학과가 필요합니다'],
+        minlength: 2,
+        maxlength: 50
+      }, 
+    prof_name: {
+        type: String,
+        required: [true, '교수성함이 필요합니다'],
+        minlength: 2,
+        maxlength: 50
+      },
+    room: {
+        type: String,
+        required: [true, '강의실이름이 필요합니다'],
+        minlength: 2,
+        maxlength: 50
+      },
+    recommend: {
+        type: Boolean,
+        default:false
+      },
+    day_and_time: {      },
+    taken_lectures: {
+        type: String
+
+      },
+    notice:{
+        type: String
+    }
+  });
+const Lecture = mongoose.model('Lecture', lectureSchema);
+
+module.exports = {Lecture};
---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+        type: String,
+        required: [true, '학생이름이 필요합니다'],
+        minlength: 2,
+        unique:1
+      },
+    user_id: {
+        type: String,
+        required: [true, '학번이 필요합니다'],
+        minlength:2,
+        maxlength:50
+      },
+    password: {
+        type: String,
+        required: [true, '비밀번호가 필요합니다'],
+        maxlength:50,
+        unique:1
+      },
+    user_grade: {
+        type: Number,
+        required: [true, '학년이 필요합니다'],
+        default:0
+      },
+    year_of_admission: {//필요?
+        type: Number,
+        required: [true, '입학년도가 필요합니다'],
+        default: 0
+      },
+    major: {
+        type: String,
+        required: [true, '전공이 필요합니다'],
+        minlength:2,
+        maxlength:50,
+      },
+    taken_lectures: {
+        type: String,
+
+      },
+    schedule: {
+        type: String,
+        required: [true, '시간표가 필요합니다'],
+      }
+  });
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
