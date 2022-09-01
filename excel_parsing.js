cf63c8f9494fb019bfce177d6e35e2012820af2e
ckrb63 <cksr1@naver.com>
2022-09-01 16:30:10 +0900

    [ADD] JSON key value mapping

--- excel_parsing.js
+++ excel_parsing.js
@@ -1,7 +1,20 @@
-const Xlsx = require( "xlsx" );
+const Xlsx = require("xlsx");
+const Lecture = require("./models/lectureModel");
 const excelFile = Xlsx.readFile("./public/node_excel.xlsx");
 const sheetName = excelFile.SheetNames[0];
 const firstSheet = excelFile.Sheets[sheetName];
-const jsonData = Xlsx.utils.sheet_to_json(firstSheet,{defval:""});
+const jsonData = Xlsx.utils.sheet_to_json(firstSheet, { defval: "" });
 
-console.log(jsonData);
+// console.log(jsonData);
+
+// const data = JSON.stringify(jsonData);
+// console.log(data);
+
+jsonData.map((data) => {
+  const newLecture = new Lecture({
+    name: data.__EMPTY_3,
+    lecture_id: data.__EMPTY_2,
+    
+  });
+});
+
