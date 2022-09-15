0dc91693c306e6c5105b378c0ef3e498a5a7dcdb
ckrb63 <cksr1@naver.com>
2022-09-15 17:31:23 +0900

    ADD 시간표에 강의 추가(검사 없이)

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,3 +1,4 @@
+const Lecture = require('../models/lectureModel');
 const Schedule = require('../models/scheduleModel');
 
 exports.getUserSchedules = async (req, res) => {
@@ -34,3 +35,30 @@ exports.postUserSchedules = async (req, res) => {
   }
 }
 
+exports.addLectureOnSchedule = async (req, res) => {
+  try {
+    const { userId, scheduleId } = req.body;
+    const currentLecture = await Lecture.findById(req.params.id);
+    const currentSchedule = await Schedule.findOne({userId,scheduleId});
+    const scheduleArray = currentSchedule.schedule;
+    scheduleArray.unshift(currentLecture);
+    console.log(scheduleArray);
+    const newSchedule = await Schedule.create({
+      userId,
+      scheduleId,
+      totalCredit:1,
+      schedule: scheduleArray
+    });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        schedule: newSchedule
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+}
--- routes/scheduleRoute.js
+++ routes/scheduleRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getUserSchedules, postUserSchedules } = require('../controllers/scheduleController');
+const { getUserSchedules, postUserSchedules, addLectureOnSchedule } = require('../controllers/scheduleController');
 
 const router = express.Router();
 
 router.route('/').get(getUserSchedules).post(postUserSchedules);
+router.route('/:id').put(addLectureOnSchedule);
 
 module.exports = router;
