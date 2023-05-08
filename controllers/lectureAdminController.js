13015a40559188c35d8d6fce242a66ac7592b240
yyunee <gimyunhe@gmail.com>
2023-05-08 13:40:09 +0900

    [FIX]adminDeleteLectures

--- controllers/lectureAdminController.js
+++ controllers/lectureAdminController.js
@@ -1,5 +1,11 @@
 const Lecture = require("../models/lectureModel");
 
+const errorGenerator = (message, statusCode = 500) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  throw error;
+};
+
 const sorting = (query, queryString) => {
   if (queryString.sort) {
     query = query.sort(queryString.sort);
@@ -30,7 +36,7 @@ exports.deleteLectures = async (req, res) => {
     const data = await Lecture.findOne(
       { year: lectureYear } && { semester: lectureSemster }
     );
-    if (!data) errorGenerator("존재하지 않는 회원입니다", 203);
+    if (!data) errorGenerator("존재하지 않습니다", 203);
     await Lecture.deleteMany(
       { year: lectureYear } && { semster: lectureSemster }
     ).then(() => {
